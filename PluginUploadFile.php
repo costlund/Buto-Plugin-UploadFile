2bee8b5ae58d8242c08356dc52b51dc4a0bd7a53
costlund <claes@webforms.se>
2018-05-17 08:58:30 +0200

    Improvements.

--- PluginUploadFile.php
+++ PluginUploadFile.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * 
+ * Improvements.
+ * 180128: Add param success/before_save to run method before save image.
+ */
 class PluginUploadFile{
   /**
    * 
@@ -23,6 +28,9 @@ class PluginUploadFile{
    * 
    */
   public function widget_element($data){
+    /**
+     * 
+     */
     $data = new PluginWfArray($data);
     $data = new PluginWfArray($data->get('data'));
     /**
@@ -40,6 +48,10 @@ class PluginUploadFile{
      */
     $fullname = wfSettings::replaceDir($data->get('dir').'/'.$data->get('name'));
     if(wfFilesystem::fileExist($fullname)){
+      if($data->get('if_file_exist/element')){
+        wfDocument::renderElement($data->get('if_file_exist/element'));
+        return null;
+      }
       /**
        * 
        */
@@ -71,6 +83,13 @@ class PluginUploadFile{
        * Handle _time param for ajax call to work.
        */
       $form = $this->getYml('element/form.yml');
+      if($data->get('btn_upload/text')){
+        //wfHelp::yml_dump($form->get('div/innerHTML/btn_select/innerHTML/a/innerHTML'));
+        $form->setById('btn_upload', 'innerHTML', $data->get('btn_upload/text'));
+        //$form->set('div/innerHTML/btn_select/innerHTML/a/innerHTML', $data->get('btn_upload/text'));
+        //wfHelp::yml_dump($form->get('div/innerHTML/btn_select/innerHTML/a/innerHTML'));
+        //wfHelp::yml_dump($form);
+      }
       $id = $data->get('id');
       $form->set('div/innerHTML/form/innerHTML/input_file/attribute/onchange', "plugin_upload_file.$id.validate();");
       $form->set('div/innerHTML/form/innerHTML/input_button/attribute/onclick', "plugin_upload_file.$id.uploadFile();");
@@ -140,6 +159,18 @@ class PluginUploadFile{
       if( strtolower(substr($fileName, (strlen($fileName)-strlen($data->get('accept'))-1)))  == strtolower('.'.$data->get('accept')) ){
         $accept = true;
       }
+      /**
+       * We also check jpeg if accept=jpg.
+       */
+      if(!$accept && strtolower($data->get('accept'))=='jpg'){
+        $data->set('accept', 'jpeg');
+        if( strtolower(substr($fileName, (strlen($fileName)-strlen($data->get('accept'))-1)))  == strtolower('.'.$data->get('accept')) ){
+          $accept = true;
+        }
+      }
+      /**
+       * 
+       */
       if(!$accept){
         $array = array();
         $array['success'] = '6';
@@ -148,9 +179,19 @@ class PluginUploadFile{
       /**
        * Copy from temp folder.
        */
+      /**
+       * Run method before save.
+       */
+      if($data->get('success/before_save/plugin') && $data->get('success/before_save/method')){
+        $data = PluginUploadFile::runCaptureMethod($data->get('success/before_save/plugin'), $data->get('success/before_save/method'), $data);
+      }
       if(move_uploaded_file($fileTmpLoc, $target_dir)){
+        /**
+         * 
+         */
         $array = array();
         $array['success'] = '1';
+        $array['capture_result'] = $data->get('capture_result');
         exit(json_encode($array));
       } else {
         exit('3');
@@ -168,6 +209,14 @@ class PluginUploadFile{
         exit("...<script>".$data->get('success/script')."</script>");
       }
     }
+  }
+  /**
+   * Capture method caller.
+   */
+  public static function runCaptureMethod($plugin, $method, $form){
+    wfPlugin::includeonce($plugin);
+    $obj = wfSettings::getPluginObj($plugin);
+    return $obj->$method($form);
   }
   /**
    * 
